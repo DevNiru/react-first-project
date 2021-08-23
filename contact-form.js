ecf653929e6c7a1736b7d759c9440c4b004cb466
DevNiru <niroshapokala26@gmail.com>
2021-08-22 23:12:36 -0400

    added git commands

--- contact-form.js
+++ contact-form.js
@@ -1,1 +1,13 @@
 // this is contact form
+
+
+
+// git status
+// git add .
+// git add  -- "filename"
+// git commit -m "commit - message" 
+// git commit -m "commit - message" -- <filename>
+// git branch
+// git push origin <current-working-branch>
+
+// git pull origin <current-working-branch>
